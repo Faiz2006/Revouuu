b1b1eac649173579e2a737db7e479696095a079b
Faizzz <106651007+Faiz2006@users.noreply.github.com>
2024-07-31 21:14:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let index = 0;
+let transitionDelay = 2000;
+
+
+let slideContainer = document.querySelector(".slideshow");
+
+let slides = slideContainer.querySelectorAll(".slide");
+
+
+for (let slide of slides) {
+  slide.style.transition = `all ${transitionDelay/1000}s linear`;
+}
+
+
+showSlide(index);
+
+
+function showSlide(slideNumber) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i == slideNumber ? "block" : "none";
+  });
+  index++;
+  // go back to 0 if at the end of slides
+  if (index >= slides.length) {
+    index = 0;
+  }
+}
+
+
+setInterval (() => showSlide(index), transitionDelay);
